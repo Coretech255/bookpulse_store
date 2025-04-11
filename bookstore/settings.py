fecdc7cf15f31fe69ef750e075730bb928d097f5
Ezechimere Wise <ikehezechi@gmail.com>
2025-04-11 11:06:56 +0100

    edited ALLOWED_HOSTS

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = config('ALLOWED_HOST', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTs', cast=Csv())
 
 
 # Application definition
