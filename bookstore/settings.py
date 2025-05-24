7a1c92b1c950399fc50b7ad7b3eb5230f7a5a2d3
Ezechimere Wise <ikehezechi@gmail.com>
2025-05-24 16:53:06 +0100

    Neon DB

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL'),
-        conn_max_age=600,
+        #conn_max_age=600,
         ssl_require=True
     )
 }
